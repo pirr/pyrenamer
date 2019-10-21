4b9a6fea891e109ba77f603d623d9a4a085c8edd
alekseysmaga <tattoo1mag@gmail.com>
2019-10-21 07:29:29 +0200

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+import argparse
+from renamer import FileRenamer
+
+parser = argparse.ArgumentParser(description='Files renaming by pattern')
+parser.add_argument('pattern', metavar='P', type=str, help='pattern for file selecting')
+parser.add_argument('rename_to', metavar='R', type=str, help='string to replacing')
+parser.add_argument('-rx', action='store_true', help='use regex')
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    rename = FileRenamer(replace=args.pattern, to=args.rename_to, with_regex=args.rx)
+    rename()
---
+++ renamer.py
@@ -0,0 +1,28 @@
+import os
+import glob
+import re
+
+
+class FileRenamer(object):
+
+    def __init__(self, replace: str, to: str, with_regex: bool) -> None:
+        self.replace = replace
+        self.to = to
+        self.with_regex = with_regex
+
+    def __call__(self) -> None:
+        cwd = os.getcwd()
+        if self.with_regex:
+            self.rx_patt = re.compile(self.replace)
+        try:
+            for file_name in os.listdir(cwd):
+                if not os.path.isfile(os.path.join(cwd, file_name)):
+                    continue
+                if self.with_regex:
+                    new_file_name = self.rx_patt.sub(self.to, file_name)
+                else:
+                    new_file_name = file_name.replace(self.replace, self.to)
+                os.rename(os.path.join(cwd, file_name), os.path.join(cwd, new_file_name))
+        except Exception as e:
+            raise(e)
+        return None
